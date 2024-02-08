3485449f42b11f1b37be04f7fd10d3638b1ceba9
SamoelFerreira <probitstecnology@gmail.com>
2024-02-08 13:50:08 -0300

    first coomit

---
+++ 4_strings/script.js
@@ -0,0 +1,5 @@
+console.log(typeof "Meu nome é Wellington");
+    console.log(typeof 'Eu estou aprendendo JavaScript');
+        console.log(typeof `Teste Drive`);
+    console.log(typeof "Infinity");
+console.log(typeof Infinity);
---
+++ 5_string_avançado/script.js
@@ -0,0 +1,5 @@
+console.log("Esta aqui é a primeira linha \n e esta aqui é a segunda linha");
+console.log("Meu nome é 'Samoel'");
+console.log('Meu nome é "Samoel"');
+console.log(`A multiplicação de 5 por 3 é ${5 * 3}`);
+console.log("Meu " + "nome " + "é " + "'Samoel'");
