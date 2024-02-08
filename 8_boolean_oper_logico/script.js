44183730dc0f41ab9469a574fb4bf4d95f802e04
SamoelFerreira <112577762+SamoelFerreira@users.noreply.github.com>
2024-02-08 19:52:07 -0300

    Add files via upload

---
+++ 8_boolean_oper_logico/script.js
@@ -0,0 +1,8 @@
+console.log(5 > 3 && 3 == 2);
+    console.log(5 > 3 || 3 == 1);
+
+                console.log(3 == 3 && "Samoel" == "Samoel");
+            console.log("Aline" == "Samoel" || false);
+        console.log(!(!(true && true)));
+    console.log(true && true);
+console.log(false || false);
